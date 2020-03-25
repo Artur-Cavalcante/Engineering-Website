ce14e25679c11c8f7619e31ea24b28f945fb9a18
Artur-Cavalcante <arturcavalcante2014.p@hotmail.com>
2020-03-25 13:31:37 -0300

    Add atribute target into tag <a>

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -15,28 +15,28 @@ function Footer() {
 
             <div className="social-media-box">
                 <div className="instagram-box">
-                    <a href="https://instagram.com/eng.ionecavalcante/">
+                    <a href="https://instagram.com/eng.ionecavalcante/" target="_blank">
                         <img src="" alt="instagram-logo" />
                         <p>Instagram</p>
                     </a>
                 </div>
 
                 <div className="e-mail-box">
-                        <NavLink to="/contato">
+                        <NavLink to="/contato"  >
                             <img src="" alt="e-mail-logo" />
                             <p>E-mail</p>
                         </NavLink>
                 </div>
 
                 <div className="phone-box">
-                        <NavLink to="/contato">
+                        <NavLink to="/contato"  >
                             <img src="" alt="phone-logo" />
                             <p>Telefone</p>
                         </NavLink>
                 </div>
 
                 <div className="github-box">
-                    <a href="http://github.com/Artur-Cavalcante/ione-cavalcante-engineering">
+                    <a href="http://github.com/Artur-Cavalcante/ione-cavalcante-engineering" target="_blank" >
                         <img src="" alt="github-logo" />
                         <p>Github</p>
                     </a>
