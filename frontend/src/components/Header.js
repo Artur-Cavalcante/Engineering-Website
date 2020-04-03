07761697eb11a3baf5240d58466c38cec6ac4448
Artur-Cavalcante <arturcavalcante2014.p@hotmail.com>
2020-04-03 10:34:18 -0300

    fix hover problems

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import '../styles/output.css';
 
@@ -6,6 +6,53 @@ import IoneCavalcanteEngenhariaLogo from './images/header/IoneCavalcanteEngineer
 import IoneCavalcanteEngenhariaLogoNameWithoutBackground from './images/header/IoneCavalcanteEngineeringLogoNameWithoutBackground.svg';
 
 function Header() {
+  const [navInicioClass, setNavInicioClass] = useState('')
+  const [navServicosClass, setNavServicosClass] = useState('')
+  const [navContatoClass, setNavContatoClass] = useState('')
+  const [navSobreClass, setNavSobreClass] = useState('')
+
+
+  function onHover(nav) {
+    switch (nav) {
+      case 'inicio':
+        setNavInicioClass(' border-b-3p border-primary');
+        break;
+      case 'servicos':
+        setNavServicosClass(' border-b-3p border-primary');
+        break;
+
+      case 'contato':
+        setNavContatoClass(' border-b-3p border-primary');
+        break;
+
+      case 'sobre':
+        setNavSobreClass(' border-b-3p border-primary');
+        break;
+      default:
+        break;
+    }
+  }
+
+  function offHover(nav) {
+    switch (nav) {
+      case 'inicio':
+        setNavInicioClass('');
+        break;
+      case 'servicos':
+        setNavServicosClass('');
+        break;
+
+      case 'contato':
+        setNavContatoClass('');
+        break;
+
+      case 'sobre':
+        setNavSobreClass('');
+        break;
+      default:
+        break;
+    }
+  }
   return (
     <header
       className="
@@ -23,11 +70,16 @@ function Header() {
           id="logo-box"
           className="
             flex flex-row items-center
+            h-full
+            w-3/5
           "
         >
           <div id="logo-img-box">
             <a href="https://ionecavalcanteengenharia.com.br">
               <img
+                className="
+                  
+                "
                 src={IoneCavalcanteEngenhariaLogo}
                 alt="Ione-Cavalcante-Engenharia-logo-img"
                 className=""
@@ -46,15 +98,56 @@ function Header() {
           </h1>
         </div>
 
-        <nav 
+        <nav
           className="
-            flex flex-row items-center justify-center 
+            flex flex-row items-center justify-around
           "
         >
-          <NavLink exact to="/" className="m-3 focus:border-gray-300 ">Início</NavLink>
-          <NavLink to="/servicos" className="m-3 border-b-3p border-primary">Serviços</NavLink>
-          <NavLink to="/contato" className="m-3">Contato</NavLink>
-          <NavLink to="/sobre" className="m-3">Sobre</NavLink>
+          <NavLink 
+            exact to="/" 
+            className={navInicioClass} 
+
+            onMouseEnter={() => onHover('inicio')} 
+            onMouseOut={() => offHover('inicio')}
+          
+            activeStyle={{
+              borderBottom: '3px solid #D49b7a',
+            }}
+          > Início</NavLink>
+          <NavLink 
+            to="/servicos"
+            className={navServicosClass} 
+
+            onMouseEnter={() => onHover('servicos')} 
+            onMouseOut={() => offHover('servicos')}
+          
+            activeStyle={{
+              borderBottom: '3px solid #D49b7a',
+            }}
+          >Serviços</NavLink>
+          <NavLink 
+            to="/contato" 
+            className={navContatoClass} 
+
+            onMouseEnter={() => onHover('contato')} 
+            onMouseOut={() => offHover('contato')}
+          
+
+            activeStyle={{
+              borderBottom: '3px solid #D49b7a',
+            }}
+          >Contato</NavLink>
+          <NavLink 
+            to="/sobre" 
+            className={navSobreClass} 
+
+            onMouseEnter={() => onHover('sobre')} 
+            onMouseOut={() => offHover('sobre')}
+          
+            activeStyle={{
+              borderBottom: '3px solid #D49b7a',
+            }}
+          >Sobre</NavLink>
         </nav>
 
       </div>
@@ -62,7 +155,7 @@ function Header() {
       <div
         id="header-bottom-bar"
         className="
-          w-full bg-primary h-1 
+          w-full bg-primary h-1
           sm:h-1
           md:h-1p
           lg:h-1p
